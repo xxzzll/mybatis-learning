37ade4b9d486af624bcf42de9f7f89f1891ecf9f
xixi <lizhenxi@sanquan.com>
2019-08-01 21:01:08 +0800

    init project

---
+++ mybatis-parent/mybatis-01-helloworld/src/main/java/com/huawei/mybatis/Employee.java
@@ -0,0 +1,27 @@
+/**
+ * FileName: Employee
+ * Author:   xixi
+ * Date:     19-8-1 下午8:44
+ * Description: 员工实体类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.huawei.mybatis;
+
+/**
+ *  <br>
+ * 〈员工实体类〉
+ *
+ * @author xixi
+ * @create 19-8-1
+ * @since 1.0.0
+ */
+public class Employee {
+    private Integer id;
+    private String lastName;
+    private String email;
+    private Integer gender;
+
+
+}
